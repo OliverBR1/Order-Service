4584198bd22414ac8a729dd1928fa9a636e4a265
OliverBR1 <Br.0liveira@outlook.com>
2026-03-29 14:47:07 -0300

    update exception

--- src/main/java/com/olivertech/orderservice/domain/exception/EventPublishingException.java
+++ src/main/java/com/olivertech/orderservice/domain/exception/EventPublishingException.java
@@ -1,6 +1,9 @@
 package com.olivertech.orderservice.domain.exception;
 
 public class EventPublishingException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     public EventPublishingException(String message, Throwable cause) {
         super(message, cause);
     }
